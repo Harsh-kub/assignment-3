825df1c03220b02b7afb2eccdb935827355112c2
Harsh-kub <harshkubade12345@gmail.com>
2025-05-26 15:55:38 +0530

    Update ASSIGNMENT 4.py

--- ASSIGNMENT 4.py
+++ ASSIGNMENT 4.py
@@ -16,4 +16,17 @@ except FileNotFoundError:
 
 #TASK 2
 
+n=input("enter the text to write to the file :")
+file1=open("output.txt","w")
+file1.write(n)
+file1.close()
+
+m=input("Enter the additional text to Append: ")
+file2=open("output.txt","a")
+file2.write(m)
+file2.close()
+
+print("The final output of file output.txt is")
+print(n)
+print(m)
 
